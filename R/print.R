2b815727bc58e2ef87837110d616922aafa70ef9
JasonQxZ <46462586+JasonQxZ@users.noreply.github.com>
2021-05-26 08:59:52 -0400

    Update

--- R/print.R
+++ R/print.R
@@ -58,8 +58,7 @@ print.selection<-function(x,...){
   catln("  ", paste(deparse(x$call), sep = "\n", collapse = "\n"))
   
   catln("Subset:")
-  
-  catln("  Feature Name: ", paste(colnames(x$X)[x$ID_selected], sep = " ", collapse = ","))
+  if( !is.null(colnames(x$X))){ catln("  Feature Name: ", paste(colnames(x$X)[x$ID_selected], sep = " ", collapse = ","))}
   catln("  Feature Index: ", paste(x$ID_selected, sep = "\n", collapse = " "))
   
   
@@ -90,7 +89,7 @@ print.summary.smle <- function(x, ...){
   catln("  Dim of Y: " , paste(x$DimY,collapse = ' x '))
   catln("  Dim of X: " , paste(x$DimX,collapse = ' x '))
   catln("  Model type: " , x$family)
-  catln("  Retained model_size : ", x$size)
+  catln("  Retained model size : ", x$size)
   
   if( !is.null(colnames(x$X))){ catln("  Feature Name: ", paste(colnames(x$X)[x$ID_retained], sep = " ", collapse = ","))}
   catln("  Feature Index: ", paste(x$ID_retained, sep = "\n", collapse = ","))
@@ -126,7 +125,7 @@ print.summary.selection <- function(x, ...){
   catln("  Recommanded feature index: ", paste(x$ID_selected, sep = "\n", collapse = ","))
   catln("  Selection criterion : ", x$criterion)
   if(x$criterion=='ebic'){  catln("  Gamma for ebic : ", as.character(x$gamma_ebic))}
-  if(x$criterion=='vote'){  catln("  Features selected by voting : ", x$ID_voted)}
+  if(x$vote=='T'){  catln("  Features selected by voting : ", x$ID_voted)}
   
   invisible(x)
 }
@@ -150,12 +149,12 @@ print.sdata<-function(x,...){
   catln(" ", paste(deparse(x$call), sep = "\n", collapse = "\n"))
   
   catln("Subset:")
-  catln(" Dim_of_Y: " , paste(c(length(x$Y),1),collapse = ' x '))
-  catln(" Dim_of_X: " , paste(dim(x$X),collapse = ' x '))
+  catln(" Dim of Y: " , paste(c(length(x$Y),1),collapse = ' x '))
+  catln(" Dim_of X: " , paste(dim(x$X),collapse = ' x '))
   catln(" Correlation: " , x$correlation)
-  catln(" Index_of_Casual_Featrues: " , paste(x$subset_true,collapse = ','))
+  catln(" Index of Causal Features: " , paste(x$subset_true,collapse = ','))
   if(x$ctg ==T){catln("Design matrix concludes categorical features" )}
-  catln(" Model_Type: ", x$family)
+  catln(" Model Type: ", x$family)
   ## done
   invisible(x)
   
